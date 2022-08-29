1db5cd013eee44ab906255f51512288edf289429
PreciousIfeaka <preciousifeaka@gmail.com>
2022-08-29 16:46:00 -0700

    printed a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for intg in range(len(row)):
-            print("{:d}".format(row[intg]), end="" if intg == len(row) - 1 else " ")
+            print(
+                    "{:d}".format(row[intg]),
+                    end="" if intg == len(row) - 1 else " ")
         print("")

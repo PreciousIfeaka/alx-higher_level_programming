627eae19c214ef8b62a20ca1dd0adfcb2e7fff49
PreciousIfeaka <preciousifeaka@gmail.com>
2022-08-29 00:56:53 -0700

    removed both cases of c from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_list = list(my_string)
-    for char in my_list:
-        if char == 'c':
-            my_list.remove('c')
-        if char == 'C':
-            my_list.remove('C')
-    my_string = "".join(my_list)
+    l = list(my_string)
+    for x in l:
+        if x == 'c':
+            l.remove('c')
+        if x == 'C':
+            l.remove('C')
+    my_string = "".join(l)
     return my_string

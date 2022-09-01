409db9af8e05f8afd5dcfd52355d688ba6e5851a
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-01 00:08:20 -0700

    returned key with max value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    else:
+        k = list(a_dictionary.keys())
+        v = list(a_dictionary.values())
+        return (k[v.index(max(v))])

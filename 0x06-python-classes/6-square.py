abc024f3ddee7a9ce83e28cf3b7c183a113184b9
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-13 23:03:58 -0700

    added a position attribute to the class

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""
+This module implements methods on class square
+"""
+
+
+class Square:
+    """empty class with size private attributes
+    Attributes:
+        size(int): dimension of the square
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        self.__size = size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+    
+    @property
+    def position(self):
+        self.__position = position
+
+    @position.setter
+    def position(self, value):
+        if type(position) is not tuple and len(position) != 2 and (
+                position[0] < 0 and position[1] < 0):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for j in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)

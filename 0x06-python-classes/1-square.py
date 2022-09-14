1651684d4dac00742e0b8b39fda0957398adc050
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-13 18:59:28 -0700

    initialized a class with attributes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module creates a class that defines a square and inits it
+"""
+
+
+class Square:
+    """This class defines a square.
+
+    Attributes:
+        size: a private instance attribute.
+
+    """
+
+    def __init__(self, size):
+        self.__size = size

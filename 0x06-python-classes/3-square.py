18c0bc9b69090b60a1da2560dc11d8404a228121
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-13 20:24:50 -0700

    created a public instance method AREA

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module creates a square class.
+"""
+
+
+class Square:
+    """This creates a class and defines it with attributes.
+    
+    Attributes:
+    size(int): An integer that defines the square dimensions.
+    """
+
+    def __init__(self, size=0):
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        elif type(size) is not int:
+            raise TypeError('size must be an integer')
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)

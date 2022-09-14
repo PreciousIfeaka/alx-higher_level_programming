aee4745526b6fe44016274ffb7510886cd372d41
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-13 19:44:18 -0700

    corrected 2-square file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,10 +10,10 @@ class Square:
     Attributes:
         size(int): describes the size of the square.
     """
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError('size must be an integer')
         elif size < 0:
-                raise ValueError('size must be >= 0')
+            raise ValueError('size must be >= 0')
         else:
             self.__size = size

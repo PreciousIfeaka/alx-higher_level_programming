8bbf012538489886e00bfd6c0e95ddb1d56d1711
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-15 18:36:10 -0700

    updated square_position file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -16,8 +16,8 @@ class Square:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -51,8 +51,8 @@ class Square:
     @position.setter
     def position(self, value):
         if isinstance(value, tuple) and len(value) == 2:
-            if isinstance(value[0], int) and isinstance(value[1], int):
-                if value[0] >= 0 and value[1] >= 0:
-                    self.__position = value
+            if all(isinstance(num, int) for num in value):
+                if all(num >= 0 for num in value):
+                    self.__position = position
         else:
-            raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError('position must be a tuple of 2 positive integers')

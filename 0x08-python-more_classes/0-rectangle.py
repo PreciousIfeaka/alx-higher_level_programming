80b06fa1264043e6b62cc8bc211b1faaa3f26d51
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-19 09:08:13 -0700

    created an empty class rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+This module contains an empty class Rectangle
+"""
+
+
+class Rectangle:
+    """This class assigns properties to a rectangle, though empty
+    """
+    pass

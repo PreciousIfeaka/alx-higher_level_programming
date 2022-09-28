c6c46375c2f82e647aa253fe83f604c3ee6af14b
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-28 08:45:18 -0700

    raised an exception for class method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This module defines an empty class
+"""
+
+
+class BaseGeometry:
+    """This class is an empty one
+    """
+    def area(self):
+        raise Exception('area() is not implemented')

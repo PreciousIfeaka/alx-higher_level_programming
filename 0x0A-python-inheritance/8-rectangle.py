9d4be7562ba564d9124bcda52a5d910bd16a436f
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-28 14:36:27 -0700

    created a new class and initialized an instance of the class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -24,10 +24,16 @@ class BaseGeometry:
 
 class Rectangle(BaseGeometry):
     """This class defines a rectangle
+        Attributes:
+            width(int): width of the rectangle
+            height(int): height of the rectangle
     """
+
     def __init__(self, width, height):
-        self.integer_validator("height", height)
+        """initializes an instance of class Rectangle"""
+        
         self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
         self.__width = width
         self.__height = height

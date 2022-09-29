50e9795c15911143c9b214ac8a7726cdddadc1ff
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-28 20:08:33 -0700

    defined the area of the rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+This module defines a function that a value in a class method
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """This class defines a rectangle
+        Attributes:
+            width(int): width of the rectangle
+            height(int): height of the rectangle
+    """
+
+    def __init__(self, width, height):
+        """initializes an instance of class Rectangle"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)

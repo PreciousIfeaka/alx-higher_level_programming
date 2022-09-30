db87f20aa82c17a0491e750983119700ea001efe
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-30 07:18:10 -0700

    deserialized a json file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,4 +11,4 @@ def load_from_json_file(filename):
     """deserializes an object from a json file"""
 
     with open(filename, 'r', encoding="utf-8") as f:
-        json.load(f)
+        return json.load(f)

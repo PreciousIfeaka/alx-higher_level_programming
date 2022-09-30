330f30a2adad71afabae65f08a83b0269260eb2a
PreciousIfeaka <preciousifeaka@gmail.com>
2022-09-30 07:14:25 -0700

    deserializes a json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This module defines a function that deserializes
+an object from a json file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """deserializes an object from a json file"""
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        json.load(f)

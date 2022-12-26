5404bf1111ee4a6a14e68fbe80e2ddf7880e3825
PreciousIfeaka <preciousifeaka@gmail.com>
2022-12-26 05:32:08 -0800

    updated scripts

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@
 SELECT tv_genres.name AS 'name', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
 FROM tv_genres RIGHT JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name
+GROUP BY name
 ORDER BY number_of_shows DESC

c0ab5d4da54f1b4f9e3837e8ec21349c9f89785f
PreciousIfeaka <preciousifeaka@gmail.com>
2022-12-26 01:21:58 -0800

    my first commit for sql query

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,13 @@
 -- creates the database 'hbtn_0d_usa' and the table 'cities' in the DB.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	PRIMARY_KEY(`id`),
-	`id` INT UNIQUE AUTO_INCREMENT NOT NULL,
-	`state_id` INT NOT NULL,
+-- use the created database
+USE hbtn_0d_usa;
+-- creates table
+CREATE TABLE IF NOT EXISTS cities (
+	PRIMARY KEY(id),
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(`state_id`)
-	REFERENCES `hbtn_0d_usa`.`states`(`id`)
+	FOREIGN KEY(state_id)
+	REFERENCES hbtn_0d_usa.states(id)
 );
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- lists all cities in the database hbtn_0d_usa.
+SELECT cities.id, cities.name, states.name
+FROM states INNER JOIN cities
+ON states.id = cities.state_id;

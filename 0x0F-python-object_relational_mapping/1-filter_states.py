55ecedf609b06f58336b1932c987e10e43ea4eb5
PreciousIfeaka <preciousifeaka@gmail.com>
2023-01-16 01:34:07 +0000

    updated file

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1],
+        password=argv[2], database=argv[3])
+    cursor = con.cursor()
+    cursor.execute(
+        "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    db = cursor.fetchall()
+    if db is not None:
+        for row in db:
+            print("({}, '{}')".format(row[0], row[1]))
+        cursor.close()
+        con.close()

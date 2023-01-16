11eb5ed65a9eafe3ff8a5858918a16ed19c28e81
PreciousIfeaka <preciousifeaka@gmail.com>
2023-01-16 16:43:33 +0000

    corrected file

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,7 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                         db=argv[3], charset="utf8")
+                         db=argv[3], charset="utf8", port=3306)
     cus = db.cursor()
     sql = "SELECT * FROM states WHERE name LIKE '{:s}'".format(argv[4])
     cus.execute(sql)

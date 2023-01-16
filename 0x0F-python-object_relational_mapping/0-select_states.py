06d9ee7bb41dafaf789dfffdd3752c077db78815
PreciousIfeaka <preciousifeaka@gmail.com>
2023-01-16 15:37:47 +0000

    corrected script error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-
+"""This script lists all states from the database hbtn_0e_0_usa
+"""
 
 import MySQLdb
 from sys import argv
 
-'''
-a script that lists all states
-from the database
-'''
+
 if __name__ == "__main__":
     con = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
-        passwd=argv[2], database=argv[3])
+        passwd=argv[2], database=argv[3], charset="utf8")
     cursor = con.cursor()
     cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
     db = cursor.fetchall()
     if db is not None:
         for row in db:
-            print("({}, '{}')".format(row[0], row[1]))
+            print(row)
         cursor.close()
         con.close()

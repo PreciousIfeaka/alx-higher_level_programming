23664faf3a573cbcf410f234230d5ad291f04958
PreciousIfeaka <preciousifeaka@gmail.com>
2023-01-17 12:22:37 +0000

    linked base model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Contains the class definition of a state and a declarative_base instance
+"""
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class, inherits from Base instance"""
+    __tablename__ = "states"
+    id = Column(Integer, nullable=False, autoincrement=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)

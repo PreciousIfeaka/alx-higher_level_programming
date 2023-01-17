d3c244c601687d629c9c8364a8d0003fe4471049
PreciousIfeaka <preciousifeaka@gmail.com>
2023-01-17 18:46:03 +0000

    updated directory

--- 0x0F-python-object_relational_mapping/1-jet.sql
+++
@@ -1,4 +0,0 @@
--- Insert and display table content
-USE test_6;
-INSERT INTO states (name) VALUES ("California");
-SELECT * FROM states ORDER BY id;
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """Contains the class definition of a state and a declarative_base instance
 """
-import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
@@ -13,6 +11,6 @@ Base = declarative_base()
 class State(Base):
     """State class, inherits from Base instance"""
     __tablename__ = "states"
-    id = Column(Integer, nullable=False, autoincrement=True,
-                primary_key=True)
+    id = Column(Integer, unique=True, nullable=False,
+                autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)

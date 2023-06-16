56627de55599903e6c816718aaa076bbb93a5630
root <root@vagrant.vm>
2023-06-16 15:00:08 +0000

    corrected control condition

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const arglen = process.argv.length - 2;
 if (arglen < 1) {
   console.log('No argument');
-} else {
+} else if (arglen === 1) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }

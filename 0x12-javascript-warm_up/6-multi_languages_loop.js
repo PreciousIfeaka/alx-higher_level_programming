f7f58ffa2fac676120ff718e1fce193bd5aef647
root <root@vagrant.vm>
2023-06-17 05:30:17 +0000

    looped through languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+
+const varArray = [myVar1, myVar2, myVar3];
+
+for (let i = 0; i < varArray.length; i++) {
+  console.log(varArray[i]);
+}

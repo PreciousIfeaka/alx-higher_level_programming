c09da14ccb7d00e05d729b3c1ea70880a660e408
root <root@vagrant.vm>
2023-07-08 17:21:03 +0000

    fetched a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module fetches a url
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        try:
+            print('Body response:')
+            print('\t- type: {}'.format(type(content)))
+            print('\t- content: {}'.format(content))
+            print('\t- utf8 content: {}'.format(content.decode('utf8')))
+        except Exception as e:
+            pass

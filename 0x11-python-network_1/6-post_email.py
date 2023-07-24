d8067e3efdd31bfa741325f4d5607a3b2af99c1b
root <root@vagrant.vm>
2023-07-24 22:06:38 +0000

    sent an email post request

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This script takes in a url and an email address, sends a POST request
+to the passed url with the email as a parameter and finally displays the
+body of the response
+"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    data = {'email': sys.argv[2]}
+
+    try:
+        response = request.post(url, data)
+        print(response.text)
+    except Exception as e:
+        pass

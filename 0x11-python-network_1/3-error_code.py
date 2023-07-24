9a5e80e30dd29376c8dc47e228d461f75224647e
root <root@vagrant.vm>
2023-07-24 20:43:33 +0000

    considered http error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,9 +11,9 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
 
-    with urllib.request.urlopen(url) as response:
-        content = response.read().decode('utf-8')
-        try:
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read().decode('utf-8')
             print(content)
-        except urllib.error.HTTPError as e:
-            print(e.code)
+    except urllib.error.HTTPError as e:
+        print(e.code)

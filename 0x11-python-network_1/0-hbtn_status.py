97be995ac35ea960f36a2932e190871f4f529f05
root <root@vagrant.vm>
2023-07-24 10:53:27 +0000

    fetched given url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""This module fetches a url
+"""
+This module fetches a url given a set of requirements.
 """
 
 import urllib.request
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         content = response.read()
         try:
             print('Body response:')
             print('\t- type: {}'.format(type(content)))
-            print('\t- content: {}'.format(content))
-            print('\t- utf8 content: {}'.format(content.decode('utf8')))
+            print('\t - content: {}'.format(content))
+            print('\t - utf8 content: {}'.format(content.decode('utf-8')))
         except Exception as e:
             pass

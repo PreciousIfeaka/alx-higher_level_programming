0537bc87c5cf21bd43a2c68e44413ef4d3f3232e
PreciousIfeaka <preciousifeaka@gmail.com>
2024-04-24 19:04:59 +0100

    corrected file

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,23 @@
+#!/usr/bin/node
 const url = process.argv[2];
-req = require('request');
+const req = require('request');
 
 req.get(`${url}`, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        let done = {};
-        let item = JSON.parse(body);
-        for (let i = 0; i < item.length; i++) {
-            let key = item[i]["userId"];
-            if (item[i]["completed"]) {
-               if (done[key]) {
-                done[key]++;
-               }
-               else {
-                done[key] = 1;
-               }
-            }
+  if (error) {
+    console.log(error);
+  } else {
+    const done = {};
+    const item = JSON.parse(body);
+    for (let i = 0; i < item.length; i++) {
+      const key = item[i].userId;
+      if (item[i].completed) {
+        if (done[key]) {
+          done[key]++;
+        } else {
+          done[key] = 1;
         }
-        console.log(done);
+      }
     }
-})
+    console.log(done);
+  }
+});
